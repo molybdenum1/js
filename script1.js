7414d72219138f919558af6862bb9085456c7652
Denis <63734039+molybdenum1@users.noreply.github.com>
2021-10-27 13:45:21 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,40 @@
+const label = document.querySelector(".label");
+
+let btn = document.querySelector(".btn");
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+
+let generAns = "";
+
+generate();
+//btn.addEventListener("click", check());
+
+function getSign(type) {
+    return type[Math.floor(Math.random() * type.length)];
+}
+function check(){
+    let inputAns = document.querySelector(".text").value;
+  if(inputAns.textContent == generAns){
+      alert("right");
+  }else{
+      alert("wrong");
+  }
+}
+
+function generate(){
+    let len = Math.random() * (4 - 2) + 2;
+    
+    for(let i = 0; i < len; i++){
+        generAns +=  genX();
+    }
+    label.innerText = generAns;
+}
+function genX(){
+    const xs = [];
+    xs.push(getSign(upperLetters));
+    xs.push(getSign(lowerLetters));
+    xs.push(getSign(numbers));
+    return getSign(xs);
+}
